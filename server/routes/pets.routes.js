1aae620ea7cc990b869fd6dc715398d8277c3190
justin-mcclain <95764373+justin-mcclain@users.noreply.github.com>
2022-05-31 19:54:57 -0500

    Delete server directory

--- server/routes/pets.routes.js
+++
@@ -1,9 +0,0 @@
-const PetController = require('../controller/pets.controller');
-
-module.exports = (app) => {
-    app.get('/api/pets', PetController.getAllPets);
-    app.get('/api/pets/:id', PetController.getOnePet);
-    app.post('/api/pets', PetController.createPet);
-    app.put('/api/pets/:id', PetController.updatePet);
-    app.delete('/api/pets/:id', PetController.deletePet);
-}
